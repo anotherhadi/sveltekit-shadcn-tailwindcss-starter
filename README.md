d2cf5b2286e9b2b5a62f196170081cfbfeab47e8
Hadi <112569860+anotherhadi@users.noreply.github.com>
2025-04-05 21:00:18 +0200

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# SvelteKit + Shadcn ui + tailwindcss starter
+
+Everything you need to build a Svelte project.
+
+## Features
+
+- [SvelteKit](https://kit.svelte.dev/): The fastest way to build Svelte apps
+- [Shadcn-svelte](https://shadcn-svelte.com/): A UI component library for Svelte
+- [Tailwind CSS](https://tailwindcss.com/): A utility-first CSS framework
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '../app.css';
+	import { Toaster } from '$lib/components/ui/sonner/index.js';
+	import { ModeWatcher } from 'mode-watcher';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+  <title>My title</title>
+  <meta
+    name="description"
+    content="My description"
+  />
+</svelte:head>
+
+<ModeWatcher />
+<Toaster />
+{@render children()}
